5b637eccb7dabc36a4525aa58dd95025683cdd16
Alex <alexeusgr@gmail.com>
2025-05-26 14:21:17 +0700

    feat: add script to monitor rad node logs and query cardano-node

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
-echo "Hello World"
+#!/bin/sh
+
+# Run rad node logs and process each line
+rad node logs |
+  while IFS= read -r line; do
+    # Check if the line contains "Received command QueryState"
+    if grep -q "Received command QueryState" <<< "$line"; then
+      # Run the command and capture the output
+      tip_info=$(nix run ~/workshop/cardano-node/.#cardano-cli -- query tip --testnet-magic 1 --socket-path "$HOME/workshop/preprod/db/node.socket")
+      # Print the message with the tip information
+      printf "QueryState command received. Current node tip:\n%s\n" "$tip_info"
+    fi
+  done
